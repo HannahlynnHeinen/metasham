fdbd0743c312bc8ff0ba98a70fdc1478fe1cdc84
SC\hannahlynn.heinen <hannahlynn.heinen@my.simpson.edu>
2017-10-31 09:56:54 -0500

    minor fix

--- metasham/public_html/login.php
+++ metasham/public_html/login.php
@@ -64,7 +64,7 @@
       </form>
     </div>
 
-        <?php include '../resources/footer.php'; ?>ooter>
+        <?php include '../resources/footer.php'; ?>
     <!-- Scripts required to make Bootstrap work better. -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
